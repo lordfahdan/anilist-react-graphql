6ee1911d3209e6233d18cc615c5cf9fe006310e3
lord <lordfahdan@gmail.com>
2023-11-18 18:17:22 +0700

    fix: add crossOrigin into google ads script

--- src/components/Adsense.js
+++ src/components/Adsense.js
@@ -10,6 +10,7 @@ const Adsense = () => {
       script.src =
         'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9838366977027267';
       script.async = true;
+      script.crossOrigin = "anonymous";
       document.head.appendChild(script);
 
       script.onload = () => {
